5d8f82f262eaa407f4be68dfbf636547c86000ba
LA <annleli@yandex.ru>
2022-09-17 15:09:17 +0300

    Ex005_if

---
+++ Ex005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите Ваше имя ");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "mari")
+{
+    Console.WriteLine ("УРАААА, это же Mari");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
